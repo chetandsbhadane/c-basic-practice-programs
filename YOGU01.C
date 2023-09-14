38b99fd928dcfa3505ea4b5cb57a635b4eb72c44
Chetan Bhadane <69416667+chetandsbhadane@users.noreply.github.com>
2023-09-14 21:04:16 +0530

    Add files via upload

---
+++ YOGU01.C
@@ -0,0 +1,96 @@
+#include<stdio.h>
+int main()
+{
+		int ch1,ch2;
+		float samt,ramt;
+		int ch;
+		
+	    convertor:
+		printf("............currency convertor...............");
+		 printf("\n1-rupee\n2-doller\n3-euro");
+		printf(" \nenter your choice....");
+		scanf("%f",&ch1);
+		printf("enter your choice convert to amount");
+	    printf("\n1-rupee\n2-doller\n3-euro");
+		printf("\nenter your choice");
+		scanf("%f",&ch2);
+		
+	if(ch1==1)
+	{
+		printf("enter your amount in rupee");
+		scanf("%d",&samt);
+		if(ch2==1)
+		{
+			ramt=samt;
+			printf("\n  %f rupee=%f rupee",samt,ramt);	
+		}
+		else if(ch2==2)
+		{
+			ramt=samt/75;
+			printf("\n  %f rupee=%f doller",samt,ramt);
+		}
+		else if(ch2==3)
+		{
+			ramt=samt/90;
+			printf("\n  %f rupee=%f euro",samt,ramt);
+		}			
+	}
+	if(ch1==2)
+     	{
+     		printf("enter your amount in doller");
+     		scanf("%d",&samt);
+     		if(ch2==1)
+     		{
+     			ramt=samt*73;
+     			printf("\n %f doller=%f rupee ",samt,ramt);
+			}
+			else if(ch2==2)
+			{
+				ramt=samt*74;
+				printf("\n  %f doller=%f doller ",samt,ramt);
+	    	}
+	    	else if(ch2==3)
+	    	{
+	    		ramt=samt*87;
+	    		printf("\n %f doller=%f euro ",samt,ramt);
+			}
+			else
+			{
+				printf("\n invalid");
+			}
+		
+	    }
+	if(ch1==3)
+	{
+	     	printf("enter your amount in euro");
+     		scanf("%d",&samt);
+     		if(ch2==1)
+     		{
+     			ramt=samt*84;
+     			printf("\n  %feuro=%f rupee ",samt,ramt);
+			}
+			else if(ch2==2)
+			{
+			   	ramt=samt*1.15;
+     			printf("\n %f euro=.2%f doller ",samt,ramt);
+			}
+			else if(ch2==3)
+			{
+				ramt=samt*84;
+     			printf("\n %f euro=%f euro ",samt,ramt);
+			}
+			else
+			{
+				printf("\n invalid");
+			}
+	}
+			printf("\n do you want to continue currency convertor");
+		    scanf("%d",&ch);
+		    if(ch==1)
+     	    {
+     	    	goto convertor;
+			}
+			printf("\n.............thank you...................");
+			
+			return 0;
+		}
